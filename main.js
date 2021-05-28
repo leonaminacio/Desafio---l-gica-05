ef9ef90b29d57a797c35f492247c78f25d4f425b
leonaminacio <leonamalves46@gmail.com>
2021-05-28 01:46:05 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,31 @@
+
+let marcas = ["Bmw", "Mercedes", "Fiat", "Audi", "Honda"]
+
+console.log(marcas)
+
+
+let carros = ["Bmw", "Mercedes", "Fiat", "Audi", "Honda"]
+
+carros.unshift("Tesla")
+
+console.log(carros)
+
+
+let marcasCarro = ["Bmw", "Mercedes", "Fiat", "Audi", "Honda"]
+
+marcasCarro.pop()
+
+marcasCarro.push("Ferrari", "Bentley")
+
+marcasCarro.shift()
+
+console.log(marcasCarro)
+
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+
+console.log(numbers)
+
+
